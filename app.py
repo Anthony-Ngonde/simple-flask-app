fcd6d2be4c092a06641f1ba59d63004f9725d183
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-09 14:34:38 +0300

    Added about.html page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,19 +6,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-    return "About Page"
+    return render_template('about.html')
+
 
 @app.route('/hello/<name>')
 def greet(name):
     return f"Hello {name}"
 
+
 @app.route('/age/<string:name>/<int:age>')
 def show_info(name,age):
     return f"{name} is {age} years old"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
