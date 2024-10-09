2b987c5cf2e6f8639466d8c5e719701b3a6ca5c4
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-09 14:29:07 +0300

    Added a few routes

--- app.py
+++ app.py
@@ -2,10 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# app.debug = True
+
 @app.route('/')
 def index():
     return "Hello"
 
+@app.route('/about')
+def about():
+    return "About Page"
+
+@app.route('/hello/<name>')
+def greet(name):
+    return f"Hello {name}"
+
+@app.route('/age/<string:name>/<int:age>')
+def show_info(name,age):
+    return f"{name} is {age} years old"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
